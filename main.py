bc946bd6c656f0ee77b50ace8401f3bfe68062a5
FrXdantas <frankrdantas@gmail.com>
2023-01-28 19:43:08 -0400

    Funcional beta 1.0

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from pathlib import Path 
+from pytube import YouTube 
+
 caminhoLista = (Path.home()/'Documentos'/'lista.txt')
 
 with open (caminhoLista,'r') as listaLer:
 
-    lista = listaLer.read()
+    lista = listaLer.readlines()
 
-    
-print (lista)
+def Download (link):
+    youtubeObjeto = YouTube(link)
+    youtubeObjeto = youtubeObjeto.streams.get_highest_resolution()
+    youtubeObjeto = youtubeObjeto
+    caminho_video = (Path.home() / 'Downloads')
+
+    try:
+        youtubeObjeto.download(caminho_video)
+    except:
+        print('Erro ao baixar video')
+    print('Download do Video do Youtube Completo')
+    print(f'Salvo em : {caminho_video}')
+
+
+for x in range(len(lista)):
+    link = lista[x]
+    print(f'{x} - {link}')
+    Download(link)
